ff548bd1b4315c62d3ebe8ee350cb01a297a73cd
TheRealYaseen <s.syedyaseen.s@gmail.com>
2024-06-15 23:46:07 +0100

    238M complete

---
+++ DSA/Leetcode/Medium/238-M-ProductExceptSelf.cs
@@ -0,0 +1,106 @@
+﻿using DSA.Leetcode.Medium;
+
+namespace DSA.Leetcode.Medium
+{
+    public class ProductExceptSelf
+    {
+        public int[] MySoln(int[] nums)
+        {
+            int[] prefix = new int[nums.Length];
+            int[] suffix = new int[nums.Length];
+
+            prefix[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                prefix[i] = prefix[i - 1] * nums[i];
+            }
+
+            suffix[nums.Length - 1] = nums[nums.Length - 1];
+            for (int i = nums.Length - 2; i > 0; i--)
+            {
+                suffix[i] = suffix[i + 1] * nums[i];
+            }
+
+            nums[0] = suffix[1];
+            nums[nums.Length - 1] = prefix[nums.Length - 2];
+
+            for (int i = 1; i < nums.Length - 1; i++)
+            {
+                nums[i] = prefix[i - 1] * suffix[i + 1];
+            }
+
+            return nums;
+        }
+
+        // for reference
+        public int[] Soln(int[] nums)
+        {
+            var res = new int[nums.Length];
+
+            int pre = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                res[i] = pre;
+                pre = nums[i] * pre;
+            }
+
+            int post = 1;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                res[i] = res[i] * post;
+                post = post * nums[i];
+            }
+            return res;
+        }
+
+
+        //Same performance,but clearer
+
+        public int[] Soln2(int[] nums)
+        {
+            // int[] result = new int[nums.Length];
+            int[] prefix = new int[nums.Length];
+            int[] suffix = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i == 0)
+                {
+                    prefix[i] = nums[i];
+                    continue;
+                }
+                prefix[i] = prefix[i - 1] * nums[i];
+            }
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (i == nums.Length - 1)
+                {
+                    suffix[i] = nums[i];
+                    continue;
+                }
+                suffix[i] = suffix[i + 1] * nums[i];
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i == 0) nums[i] = suffix[i + 1];
+                else if (i == nums.Length - 1) nums[i] = prefix[i - 1];
+                else nums[i] = prefix[i - 1] * suffix[i + 1];
+            }
+
+            return nums;
+        }
+    }
+}
+
+
+//var cl = new ProductExceptSelf();
+//var a = cl.Soln(new int[] { 1, 2, 3, 4 });
+//// [1, 2, 6, 24]
+//// [24, 24, 12, 4]
+
+//// [24, 12, 8, 6]
+
+//var b = cl.Soln(new int[] { -1, 1, 0, -3, 3 });
+//// [0,0,9,0,0]
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,117 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Collections;
-using System.Diagnostics;
-using System.Xml;
-using DSA.ArrayDSA;
-using DSA.AVLTreeDS;
-using DSA.DictionaryDSA;
-using DSA.GraphDS;
-using DSA.HeapDSA;
-using DSA.LinkedListDSA;
-using DSA.QueueDSA;
-using DSA.StackClassDSA;
-using DSA.TreeDS;
 using DSA.Leetcode;
+using DSA.Leetcode.Medium;
 
 // var Soln = new ValidAnagram();
-// Console.WriteLine(Soln.isAnagram("ba", "ab"));
-// Console.WriteLine(Soln.isAnagram("rat", "car"));
-var Soln = new GroupAnagramsSoln();
-var ans = Soln.GroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" });
+var cl = new ProductExceptSelf();
+var a = cl.Soln(new int[] { 1, 2, 3, 4 });
+// [1, 2, 6, 24]
+// [24, 24, 12, 4]
 
-Console.WriteLine(Soln.GroupAnagrams(new string[] {""}));
-var ans1 = Soln.GroupAnagrams(new string[] { "ac", "c" });
-// var ans1 = Soln.GroupAnagrams(new string[] { "" });
-Console.WriteLine("This");
+// [24, 12, 8, 6]
 
-// Console.WriteLine(Soln.GroupAnagrams(new string[] {"a"}));
-// Console.WriteLine(Soln.GroupAnagrams(new string[] {"eat","tea","tan","ate","nat","bat"}));
-
-
-
-
-
-// System.Console.WriteLine(Soln.InsertPos(nums, 4));
-// var dict = new DictionaryClass();
-// dict.Put(5, "Yaseen");
-// dict.Put(5, "Angry");
-// dict.Put(1, "Syed");
-// dict.Put(10, "POtato");
-// Console.WriteLine(dict.Get(5));
-
-// var repeatChar = new RepeatedCharacter();
-// char[] arr = new[] { 'a', 'b', 'c', 'a', 'b', 'a', 'a' };
-// Console.WriteLine(repeatChar.FindMostRepeatedElement(arr));
-
-// int[] arrInt = new[] {1, 7, 5, 9, 2, 12, 3};
-// Console.WriteLine(repeatChar.CountPairsWithDiff(arrInt, 2));
-// var Tree = new TreeClass();
-// var Tree1 =  new TreeClass();
-// var Tree2 =  new TreeClass();
-
-// var list1 = new List<int> { -5, 20, 10, 30, 6, 14, 24, 3, 8, 26};
-// var list2 = new List<int> { -5, 20, 10, 30, 6, 14, 24, 3, 8, 26};
-// var list = new List<int> { 3, 2, 5 };
-
-// list.ForEach(item => Tree1.Insert(item));
-// list2.ForEach(item => Tree2.Insert(item));
-// Tree = Tree1;
-
-//Console.WriteLine(Tree.Find(5));
-// List<int> result = Tree.DepthPreOrderTraversal();
-// result.ForEach(x => Console.WriteLine(x));
-//Tree.DepthInOrderTraversal();
-//Tree.DepthPostOrderTraversal();
-//Console.WriteLine(Tree.Height());
-// Console.WriteLine(Tree.Min());
-// Console.WriteLine(Tree1.Equality(Tree2));
-
-// Tree1.SwapNodes();
-// Console.WriteLine(Tree1.isBinarySearchTree());
-
-
-// var result = Tree.NodeAtKLevel(2);
-// result.ForEach(x => Console.WriteLine(x));
-
-
-// var list = new List<int> {10, 5, 15, 6, 1, 8, 12, 18, 17 };
-
-// var list = new List<int> {10, 20, 30 };
-// var avl = new AVLTree();
-// list.ForEach(x => avl.Insert(x));
-// avl.Insert(200);
-
-
-// var heap = new HeapClass();
-
-// heap.Insert(15);
-// heap.Insert(10);
-// heap.Insert(3);
-// heap.Insert(8);
-// heap.Insert(12);
-//
-// heap.Insert(9);
-// heap.Insert(4);
-// heap.Insert(1);
-// heap.Insert(24);
-
-// heap.Insert(10);
-// heap.Insert(5);
-// heap.Insert(17);
-// heap.Insert(4);
-// heap.Insert(22);
-//
-// heap.Remove();
-// List<int> heapList = new List<int> { 4, 1, 24};
-//
-// heapList.ForEach(el => heap.Insert(el));
-
-
-
-
-// heap._heap.ForEach(el => Console.WriteLine(el));
-// Console.Write("This");
-
-
+var b = cl.Soln(new int[] { -1, 1, 0, -3, 3 });
+// [0,0,9,0,0]
 
