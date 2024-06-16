9bf856164672684f7e8e27b34b743ce7138cef03
TheRealYaseen <s.syedyaseen.s@gmail.com>
2024-06-16 23:35:59 +0100

    count sort incomplete

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -2,17 +2,33 @@
 
 using DSA.Leetcode;
 using DSA.Leetcode.Medium;
+using DSA.SortAlgo;
+using System.Collections.Generic;
 
-var cl = new EncodeAndDecodeStrings();
+//var cl = new EncodeAndDecodeStrings();
 
-var inputs = new List<List<string>>()
+//var inputs = new List<List<string>>()
+//{
+//    new List<string>() {"neet","code","love","you"},
+//    new List<string>() { "we", "say", ":", "yes" }
+
+//};
+
+//foreach (var i in inputs)
+//{
+//    cl.Soln(i);
+//}
+
+var cl = new CountSort();
+
+var inputs = new List<int[]>()
 {
-    new List<string>() {"neet","code","love","you"},
-    new List<string>() { "we", "say", ":", "yes" }
+    new int[] {5,3,8,5,1,2,8}
+    
 
 };
 
 foreach (var i in inputs)
 {
-    cl.Soln(i);
+    cl.Srt(i);
 }
--- DSA/SortAlgo/CountSort.cs
+++ DSA/SortAlgo/CountSort.cs
@@ -2,5 +2,38 @@
 {
     public class CountSort
     {
+        public int[] Srt(int[] nums)
+        {
+            var result = new int[nums.Length];
+
+            var highest = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > highest) highest = nums[i];
+            }
+
+
+            var freq = new int[highest + 1];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                freq[nums[i]]++;
+            }
+
+            for(int i=0; i < highest; i++)
+            {
+                for(int j = freq[i]; j > 0; j--)
+                {
+                    result[i] = i;
+                }
+            }
+            // Create array that has size of largest value in arr
+            // Get the frequency of each number
+            // Index = value of the number - Value = freq
+            // Iterate through the arr and place the numbers on result
+
+
+
+            return result;
+        }
     }
 }
