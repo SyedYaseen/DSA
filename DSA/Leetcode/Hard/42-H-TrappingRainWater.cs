c8e7a3e8cc6cdee90176f2c2a5373c9d87e2cc65
TheRealYaseen <s.syedyaseen.s@gmail.com>
2024-06-20 10:30:50 +0100

    in prgress 42H

--- DSA/Leetcode/Hard/42-H-TrappingRainWater.cs
+++ DSA/Leetcode/Hard/42-H-TrappingRainWater.cs
@@ -18,43 +18,41 @@ namespace DSA.Leetcode.Hard
             // Initial l needs to be incremented until the next item
             // is not higher than the current one
 
+            while (l + 1 < lastIndex)
+            {
+                if (height[l + 1] < height[l]) { r = l + 1; break; }
+                else l++;
+            }
+
+            /* 
+            After l is found, inc r until value increases for r compared to previous value
+            Also incrase r if its right next to l, because water can exist there
+            */
+            int blocks = 0;
             while (r < lastIndex)
             {
-                while (l + 1 < lastIndex)
+                if (r + 1 < lastIndex && (height[r + 1] < height[r] || ) // height[r] < height[l] &&
                 {
-                    if (height[l + 1] < height[l]) { r = l + 1; break; }
-                    else l++;
+                    blocks += height[r];
+                    r++;
                 }
-
-                /* 
-                 After l is found, inc r until value increases for r compared to previous value
-                 Also incrase r if its right next to l, because water can exist there
-                */
-                int blocks = 0;
-                while (r + 1 < lastIndex)
+                else
                 {
+                    r++;
+                    int maxWallSize = Math.Min(height[l], height[r]);
+                    blocks += maxWallSize;
+                    result = ((r - l) * maxWallSize) - blocks;
 
-                    if (height[r + 1] < height[r]) // height[r] < height[l] &&
-                    {
-                        blocks += height[r];
-                        r++;
-                    }
-                    else
-                    {
-                        r++;
-                        int maxWallSize = Math.Min(height[l], height[r]);
-                        blocks += maxWallSize;
-                        result = ((r - l) * maxWallSize) - blocks;
-
-                        // set new l value for next set
-                        l = r;
-                        blocks = 0;
-                        continue;
-                    }
+                    // set new l value for next set
+                    l = r;
+                    while (l + 1 < lastIndex && height[l] == height[l + 1]) l++;
+                    r = l + 1;
+                    blocks = 0;
                 }
-
-                //return result;
             }
+
+               
+            
             return result;
         }
     }
