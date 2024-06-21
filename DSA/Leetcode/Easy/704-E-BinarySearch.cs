26a1eb759aa0d12cd0e91f999110971c0c3ab8a4
TheRealYaseen <s.syedyaseen.s@gmail.com>
2024-06-21 13:44:46 +0100

    Daily temp and bin search

---
+++ DSA/Leetcode/Easy/704-E-BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Leetcode.Easy
+{
+    public class BinarySearch
+    {
+        public int Soln(int[] nums, int target)
+        {
+            int min = 0;
+            int max = nums.Length - 1;
+
+
+            while (min <= max)
+            {
+                int middle = min + (max - min ) / 2;
+               
+                if (target > nums[middle])
+                    min = middle + 1;
+                else if (target < nums[middle])
+                    max = middle - 1;
+                else
+                    return middle;
+
+            }
+            return -1;
+        }
+    }
+}
--- DSA/Leetcode/Medium/739-M-DailyTemperatures.cs
+++ DSA/Leetcode/Medium/739-M-DailyTemperatures.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DSA.Leetcode.Medium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,17 +9,69 @@ namespace DSA.Leetcode.Medium
 {
     public class DailyTemperatures
     {
-        public int[] Soln2(int[] temperatures)
+        public int[] Soln(int[] temperatures)
         {
+            var result = new int[temperatures.Length];
+            if (temperatures.Length == 0) return [];
+            if (temperatures.Length == 1) return [0];
+
             var st = new Stack<int>();
+            
+            st.Push(0);
+            for (int i = 1; i < temperatures.Length; i++)
+            {
+                // If current temp > top of stack
+                // Add the current index - index of the popped element
+                while (st.Count != 0 && temperatures[i] > temperatures[st.Peek()])
+                {
+                    var index = st.Pop();
+                    result[index] = i - index;
+                }
 
+                if (st.Count == 0 || temperatures[i] <= temperatures[st.Peek()])
+                {
+                    st.Push(i);
+                }
+            }
+            result[temperatures.Length - 1] = 0;
+            return result;
+        }
 
+        // Works but bad perfomance
+        public int[] Soln3(int[] temperatures)
+        {
+            var result = new int[temperatures.Length];
+            if (temperatures.Length == 0) return [];
+            if (temperatures.Length == 1) return [0];
 
+            var st = new Stack<Dictionary<int, int>>();
+
+            st.Push(new Dictionary<int, int>() { { temperatures[0], 0 } });
+            for (int i = 1; i < temperatures.Length; i++)
+            {
+                // If current temp > top of stack
+                // Add the current index - index of the popped element
+                while (st.Count != 0 && temperatures[i] > st.Peek().First().Key)
+                {
+                    var top = st.Pop().First();
+                    var key = top.Key;
+                    var index = top.Value;
+                    result[index] = i - index;
+                }
+
+                if (st.Count == 0 || temperatures[i] <= st.Peek().First().Key)
+                {
+                    st.Push(new Dictionary<int, int>() { { temperatures[i], i } });
+                }
+
+            }
+            result[temperatures.Length - 1] = 0;
+            return result;
         }
 
 
-            // Fails for an absurd testcase
-            public int[] Soln2(int[] temperatures)
+        // Fails for an absurd testcase
+        public int[] Soln2(int[] temperatures)
         {
             if (temperatures.Length <= 1) return [0]; 
             int i = 0;
@@ -45,3 +98,16 @@ namespace DSA.Leetcode.Medium
         }
     }
 }
+
+//var cl = new DailyTemperatures();
+
+//var a = cl.Soln([73, 74, 75, 71, 69, 72, 76, 73]);
+////[1,1,4,2,1,1,0,0]
+//var b = cl.Soln([30, 40, 50, 60]);
+////[1, 1, 1, 0]
+//var c = cl.Soln([30, 60, 90]);
+//// [1,1,0]
+//var d = cl.Soln([99, 99, 99]);
+
+//var e = cl.Soln([89, 62, 70, 58, 47, 47, 46, 76, 100, 70]);
+//// [8,1,5,4,3,2,1,1,0,0]
