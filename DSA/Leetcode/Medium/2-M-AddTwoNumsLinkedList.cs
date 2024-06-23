50d2f6cf1e1bf37a7398caac144bdfaf0acd0210
TheRealYaseen <s.syedyaseen.s@gmail.com>
2024-06-23 22:12:55 +0100

    2M complete

--- DSA/Leetcode/Medium/2-M-AddTwoNumsLinkedList.cs
+++ DSA/Leetcode/Medium/2-M-AddTwoNumsLinkedList.cs
@@ -11,44 +11,26 @@ namespace DSA.Leetcode.Medium
         public ListNode Soln(ListNode l1, ListNode l2)
         {
             int carry = 0;
-            ListNode left = l1;
-            ListNode lastL1 = null;
-            while (left != null && l2 != null)
+ 
+            
+            ListNode result = new ListNode(0);
+            ListNode tail = result;
+
+            while (l1 != null || l2 != null || carry == 1)
             {
-                var res = carry + left.val + l2.val;
+                var res = carry + (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
                 carry = res > 9 ? 1 : 0;
-                left.val = res % 10;
+                res = res % 10;
 
-                if ( left.next != null ) lastL1 = left.next;
-                left = left.next;
-                l2 = l2.next;
+                tail.next = new ListNode(res);
+                tail = tail.next;
+
+
+                l1 = l1?.next;
+                l2 = l2?.next;
             }
 
-            if (l2 != null)
-            {
-                lastL1.next = l2;
-                left = lastL1.next;
-            }
-
-
-
-            while (carry != 0)
-            {
-
-                var res = carry + left.val;
-                if (res < 10) carry = 0;
-                left.val = res % 10;
-
-                if (left.next != null) left = left.next;
-                else
-                {
-                    left.next = new ListNode(carry);
-                    carry = 0;
-                }
-            }
-
-
-            return l1;
+            return result.next;
         }
 
         public ListNode Soln2(ListNode l1, ListNode l2)
