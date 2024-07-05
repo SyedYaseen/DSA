da953942b016ff1cfe0fef1a7305fe05103c9808
TheRealYaseen <s.syedyaseen.s@gmail.com>
2024-07-06 00:20:09 +0100

    until 7:53:43

--- CPP/OOP.cpp
+++ CPP/OOP.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include "User.cpp"
 #include "Phone.h"
+#include "Rectangle.h"
 using namespace std;
 int main() {
 	//User yaseen;
 	//yaseen.name = "Yaseen";
 	//yaseen.printName();
-	Phone onePlus;
+	
+	//Inheritance, Polymorphism
+	//Phone p1;
+	Phone onePlus("OP7", "oxy");
+	
+	onePlus.MakeCall();
 
+	Phone iPhone = onePlus;
+
+	std::cout << iPhone.getName() << endl;
+
+	//Rectangle r1(2, 2);
+	//Rectangle r2(3, 3);
+	//cout << r2.Compare(r1);
 
 	return 0;
 }
--- CPP/Phone.h
+++ CPP/Phone.h
@@ -1,17 +1,33 @@
 #pragma once
-#include <string>
-#include <iostream>
-using namespace std;
+#ifndef PHONE_H
+#define PHONE_H
 
-class Phone
-{
-	string _name;
-	string _os;
+#include <iostream>
+#include <string>
+
+class Phone {
+    std::string _name;
+    std::string _os;
+    //Phone(); //Adding default construvtor here will make this unacessible
 
 public:
-	Phone() {
-		cout << "This default";
-	}
-	
+    Phone() {
+        std::cout << "This default\n";
+    }; // Default constructor
+    Phone(const std::string& name, const std::string& os); // Param constructor
+    Phone(const Phone& phone);
+    virtual void MakeCall();
+    std::string getName() { return _name; }
+    ~Phone() {
+        std::cout << "Destructor called for " << _name << std::endl;
+    }
 };
 
+
+class SmartPhone : public Phone {
+public:
+    void MakeCall() {
+        std::cout << "Inside smart Phone\n";
+    }
+};
+#endif // PHONE_H
