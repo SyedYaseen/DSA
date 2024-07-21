b5a050cc8c62971308eaa0f6f6b74dbbf06f8dca
TheRealYaseen <s.syedyaseen.s@gmail.com>
2024-07-21 10:29:11 +0100

    changs

--- CPP/LeetCode/main.cpp
+++ CPP/LeetCode/main.cpp
@@ -2,9 +2,11 @@
 #include <vector>
 #include "Tree.h"
 using namespace std;
-#include "MinPenalty-2843-M.h"
+#include "./Medium/DecodeStringAtIndex-880-M.h"
 int Leet() {
 	Solution s;
+
+	s.decodeAtIndex("y959q969u3hb22odq595", 222280369);
 
 	//TreeNode* t1 = new TreeNode(4, 
 	//	new TreeNode(2, 
@@ -23,7 +25,7 @@ int Leet() {
 	//int a = s.isBalanced(t2);
 	//string x = s.decodeString("100[leetcode]");
     //int a = s.bestClosingTime("YYNY");
-	int a = s.bestClosingTime("NYNNNYYN"); // ans 0
+	//int a = s.bestClosingTime("NYNNNYYN"); // ans 0
 	
 	//string a = s.decodeString("zxy3[a]2[bc]");
 	//string b = s.decodeString("zxy3[a2[c]]");
