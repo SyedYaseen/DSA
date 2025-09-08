10bd97b9b5069bbffbec9618d1f405bc7a26553f
syedyaseen <s.syedyaseen.s@gmail.com>
2025-09-08 18:27:58 +0100

    rustFiles

---
+++ CSES/rust/src/bin/MovieFestivalQueries1664.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn main() {
+    let path = "../CSES/tests/AdditionalProblems/MovieFestivalQueries1664/1.in";
+    let file = File::open(path).expect("Failed to open input file");
+    let mut reader = BufReader::new(file);
+
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+    let n: i64 = line.trim().parse().unwrap();
+
+    println!("n = {}", n);
+}
